510f80a587f38b6cdc3da661d8e3019e8d708d98
Mykyta Krainik <nikinta2003@gmail.com>
2024-01-17 11:43:12 +0200

    refactor: rewrite main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,7 @@
 #include <iostream>
-#include <fstream>
-#include "inverted_index.h"
-#include "thread_pool_lib/thread_pool.h"
-
-using std::ifstream;
 
 int main() {
-    word test_word = L"example";
-    document test_doc = "doc1";
-    auto* index = new inverted_index();
-
-    index->add(test_word, test_doc);
-    index->add(test_word, "doc2");
-    index->add(test_word, "doc3");
-    index->add(L"example2", "doc1");
-
-    string file_path = "/home/mykyta/uni/PC/inverted-index/data/test_index.json";
-
-    index->save_as_json(file_path);
-
-    ifstream file(file_path);
-
-    file.close();
+    std::cout << "Hello, it's a course work!" << std::endl;
 
     return 0;
 }
